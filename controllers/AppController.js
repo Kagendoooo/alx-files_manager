f69420557866d817a3142885515e5516d4c54107
Kamundia-Simon <simonkamundia8@gmail.com>
2024-12-10 21:56:39 +0300

    API edpoints

---
+++ controllers/AppController.js
@@ -0,0 +1,31 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+//GET Redis Status
+class AppController {
+  static async getStatus(req, res) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    res.status(200).json(status);
+  }
+
+  //GET stats
+  static async getStats(req, res) {
+    try {
+      const usersCount = await dbClient.nbUsers();
+      const filesCount = await dbClient.nbFiles();
+      const stats = {
+        users: usersCount,
+        files: filesCount,
+      };
+      res.status(200).json(stats);
+    } catch (error) {
+      console.error('Error fetching stats:', error.message);
+      res.status(500).json({ error: 'An Error occcured in fetching stats' });
+    }
+  }
+}
+
+export default AppController;

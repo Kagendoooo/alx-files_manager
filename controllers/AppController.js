175227dc18433a588c98e271fb498f87baf1b4f2
Gendozzzz <sunflowermaina@gmail.com>
2024-12-10 23:16:38 +0300

    Fix eslint

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,17 +1,17 @@
-import redisClient from '../utils/redis';
+import RedisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-//GET Redis Status
+// GET Redis Status
 class AppController {
   static async getStatus(req, res) {
     const status = {
-      redis: redisClient.isAlive(),
+      redis: RedisClient.isAlive(),
       db: dbClient.isAlive(),
     };
     res.status(200).json(status);
   }
 
-  //GET stats
+  // GET stats
   static async getStats(req, res) {
     try {
       const usersCount = await dbClient.nbUsers();

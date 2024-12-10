61bd3ccaceebbc2112973b2656932d3be23a01eb
Kamundia-Simon <simonkamundia8@gmail.com>
2024-12-11 01:05:05 +0300

    files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/files', FilesController.postUpload);
 
 export default router;

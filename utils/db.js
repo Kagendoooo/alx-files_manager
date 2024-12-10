84dde1f9f42d37ff04d34b3d8798614912e70a44
Kamundia-Simon <simonkamundia8@gmail.com>
2024-12-10 21:13:43 +0300

    db eslint fix

--- utils/db.js
+++ utils/db.js
@@ -7,7 +7,7 @@ class DBClient {
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}`;
-    
+
     this.client = new MongoClient(url, { useUnifiedTopology: true });
     this.dbName = database;
     this.isConnected = false;
@@ -24,12 +24,12 @@ class DBClient {
       });
   }
 
-  //Checks if the MongoDB client connection
+  // Checks if the MongoDB client connection
   isAlive() {
     return this.isConnected;
   }
 
-  //Gets the number of documents in the users
+  // Gets the number of documents in the users
   async nbUsers() {
     try {
       const db = this.client.db(this.dbName);
@@ -41,7 +41,7 @@ class DBClient {
     }
   }
 
-  //Gets the number of documents in files
+  // Gets the number of documents in files
   async nbFiles() {
     try {
       const db = this.client.db(this.dbName);
